b30b740cca5cdbcfe9d96df8ca1af520a4ccff8a
wrtrsblck <116472718+writersbl0ck@users.noreply.github.com>
2022-10-27 05:19:41 +0000

    created functions for calculating motion, moved functions to function.h.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
+// Credit to Dr. T for naming and formatting ideas
 #include <iostream>
-//#include <stdlib>
 #include <iomanip>
-#include "Input_Validation_Extended.h"
+#include "functions.h"
 
-// function prototypes
-void showMenu();
-void handleOption(string);
 
 
 int main() {
@@ -17,61 +14,11 @@ int main() {
   do{
     showMenu();
     cout << color;
-    option = validateString(option);
+    //option = validateString(option);
+    cin >> option;
     cout << reset;
     handleOption(option);
   }while(option != "e" && option != "E");
 
   return 0;
 }
-
-// function definitions
-void showMenu(){
-  cout << "\nMenu" << endl;
-  cout << "V = Calculate Velocity" << endl;
-  cout << "A = Calculate Acceleration" << endl;
-  cout << "M = Calculate Motion" << endl;
-  cout << "N = Calculate Newton's 2nd Law" << endl;
-  cout << "W = Calculate Weight" << endl;
-  cout << "P = Calculate Momentum" << endl;
-  cout << "X = Clear Screen" << endl;
-  cout << "E = Exit" << endl;
-}
-
-void handleOption(string userOption){
-  double distance = 0.0;
-  double time = 0.0;
-  
-  if(userOption == "V" || userOption == "v"){
-    cout << "Velocity" << endl;
-  }
-  else if(userOption == "A" || userOption == "a"){
-    cout << "Acceleration" << endl;
-  }
-  else if(userOption == "M" || userOption == "m"){
-    cout << "Motion" << endl;
-  }
-  else if(userOption == "N" || userOption == "n"){
-    cout << "Newton's 2nd Law" << endl;
-  }
-  else if(userOption == "W" || userOption == "w"){
-    cout << "Weight" << endl;
-  }
-  else if(userOption == "P" || userOption == "p"){
-    cout << "Momentum" << endl;
-  }
-  else if(userOption == "X" || userOption == "x"){
-    system("clear");
-  }
-  else if(userOption == "E" || userOption == "e"){
-    cout << "See ya!" << endl;
-  }
-  else{
-    cout << "Invalid Input" << endl;
-  }
-}
-
-
-
-
-
